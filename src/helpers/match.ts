dcdb5ca6ce21da65a15ac37d5d409da499f4d2ed
Oleg V. Kuzmin <inpassor@yandex.com>
2020-02-13 13:15:58 +0300

    improvements

--- src/helpers/match.ts
+++ src/helpers/match.ts
@@ -1,16 +1,18 @@
 export const match = (pattern: string, string: string): { [key: string]: string | number } => {
     const regex = pattern
         .replace(/</g, '(?<')
-        .replace(/\|d>/g, '>[0-9]+)')
-        .replace(/\|s>/g, '>[a-zA-Z]+)')
-        .replace(/>(?!\[)/g, '>[a-zA-Z0-9]+)');
-    const execResult = new RegExp(regex).exec(string);
+        .replace(/\(\?<\//g, '/?(?<')
+        .replace(/\|\?>/g, '>[a-zA-Z0-9-_]*)')
+        .replace(/\|n>/g, '>[0-9]+)')
+        .replace(/\|l>/g, '>[a-zA-Z]+)')
+        .replace(/>(?!\[)/g, '>[a-zA-Z0-9-_]+)');
+    const execResult = new RegExp(`^${regex}$`).exec(string.replace(/^\/+|\/+$/g, ''));
     const result: { [key: string]: string | number } = (execResult && { ...execResult.groups }) || null;
     if (result) {
-        const decimalKeys = pattern.match(/<([a-zA-Z0-9]+)\|d>/g);
+        const decimalKeys = pattern.match(/<([a-zA-Z0-9]+)\|n>/g);
         if (decimalKeys && decimalKeys.length) {
             decimalKeys.forEach(decimalKey => {
-                const key = decimalKey.replace('<', '').replace('|d>', '');
+                const key = decimalKey.replace('<', '').replace('|n>', '');
                 if (result[key]) {
                     result[key] = +result[key];
                 }
